b5bd6ef74b84414f5dbb350322c300f05565f8ba
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-09-28 21:59:00 +0300

    implement py-person-class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+        Person(person["name"], person["age"])
+
+    for person in people:
+        person_inst = Person.people[person["name"]]
+        if "wife" in person and person["wife"] is not None:
+            person_inst.wife = Person.people[person["wife"]]
+        if "husband" in person and person["husband"] is not None:
+            person_inst.husband = Person.people[person["husband"]]
+
+    return list(Person.people.values())
