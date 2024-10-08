629054e2da08bdb3ff16e21cbc8240f3ea023138
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-10-08 20:39:14 +0300

    fixed according to the comments

--- app/main.py
+++ app/main.py
@@ -8,14 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    for person in people:
-        Person(person["name"], person["age"])
+    [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         person_inst = Person.people[person["name"]]
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife"):
             person_inst.wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband"):
             person_inst.husband = Person.people[person["husband"]]
 
     return list(Person.people.values())
